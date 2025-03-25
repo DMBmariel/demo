814f3a31559a31971f4f69bd76bb8b2199dacd12
Diana Mariel Belmonte <dmbmariel@gmail.com>
2025-03-24 21:39:35 -0300

    Punto 7 - inciso 3

--- questions.py
+++ questions.py
@@ -23,18 +23,17 @@ correct_answers_index = [1, 2, 0, 3, 1]
 # inicializamos el puntaje obtenido
 score = 0
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
- # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions)-1)
- # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
-        print(f"{i + 1}. {answer}")
+questions_to_ask = random.choices(list(zip(questions,
+ answers, correct_answers_index)), k=3)
+for i in range(3):
+    print(questions_to_ask[i][0])
+    for j, answer in enumerate(questions_to_ask[i][1]):
+        print(f"{j+1}.{answer}")
     correct_answer = False
- # El usuario tiene 2 intentos para responder correctamente
+# El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
         user_answer = input("Respuesta: ")
- # verifico que el numero ingresado sea valido 
+# verifico que el numero ingresado sea valido 
         if user_answer.isdigit():
             user_answer = int(user_answer)
             if (user_answer >= 1 and user_answer <= 4):
@@ -44,19 +43,21 @@ for _ in range(3):
         else:
             print('Respuesta no valida')
  # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == questions_to_ask[i][2]:
                print("¡Correcto!")
                score += 1
                correct_answer = True
                break
         else:
-            score -= 0.5 
+            score -= 0.5  
     # Si el usuario no responde correctamente después de 2 intentos,
     # se muestra la respuesta correcta
     if correct_answer == False:
-        print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index] [correct_answers_index[question_index]])
+        question, answer, correct_index = questions_to_ask[i]
+        print(f"Incorrecto. La respuesta correcta es: {answer[correct_index]}")
+          
  # Se imprime un blanco al final de la pregunta
     print()
 # Se imprime el puntaje obtenido del juego
 print (f" El puntaje obtenido es: {score}") 
+
